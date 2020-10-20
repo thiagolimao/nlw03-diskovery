f8e7791db4d1edf0fb9957354d40b61a1d01473b
Thiago Limão <thiago@thiagolima.com>
2020-10-20 19:29:02 -0300

    Alteracao do nome do orfanato

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -20,7 +20,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+}).setContent('Gotas de Flor com amor <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
 
 
 // Create and add marker
